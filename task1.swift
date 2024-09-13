23b7f4a126e978f60ffda6cc9e401ef1c13c059a
Nikita <nikita.004.nikita@gmal.com>
2024-09-13 14:01:41 +0500

    first commit

---
+++ task1.swift
@@ -0,0 +1,15 @@
+let currentYear = 2024
+var firstName = "Nikita"
+var lastName = "Andryuchshenko"
+var age = currentYear-birthYear;
+var birthYear = 2004
+var isStudent = true
+var height = 182
+var hobby = "electricalEngineering"
+var numberOfHobbies = 4
+var favoriteNumber = 134
+var isHobbieCreative = false
+var story = "My name is \(firstName) \(lastName). I am \(age) years old,born in \(birthYear). I am currently a student. I enjoy \(hobby), which is not a creative hobby. I have \(numberOfHobbies) in total, and my favorite number is \(favoriteNumber). "
+var future = "In future i wanna be a superman"
+story.append(future)
+print(story)
