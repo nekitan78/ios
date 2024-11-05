eb3fba18fbf3d8aa37e7908d7e842223ada11a27
nekitan78 <123075856+nekitan78@users.noreply.github.com>
2024-11-05 13:07:25 +0500

    TableviewFinal
    Movies

---
+++ moviesTop/moviesTop/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  moviesTop
+//
+//  Created by Student on 01.11.2024.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var desc: UILabel!
+    @IBOutlet weak var text: UILabel!
+    
+    var inputModel: Movie?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        image.image = inputModel?.poster
+        desc.text = inputModel?.title
+        text.text = inputModel?.description
+        // Do any additional setup after loading the view.
+    }
+    
+    func configure(with model: Movie?){
+        self.inputModel = model
+        
+    }
+
+}
